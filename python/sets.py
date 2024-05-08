a18851042d669592848c3753c541f167657b888b
Sergio <snab927@gmail.com>
2024-05-08 14:02:21 -0500

    sets

---
+++ python/sets.py
@@ -0,0 +1,14 @@
+# Create an empty set
+s = set()
+
+# Add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+print(s)
+
+# no element ever appears twice in the set by definition
+s.remove(2)
+print(s)
